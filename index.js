8d16d74b3eb439ee05f7135180eb00fb0e0107b1
scout.burghardt@gmail.com <scout.burghardt@gmail.com>
2018-06-11 17:10:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (cardNumber) {
 
-    return "Your total cost is $" + total() + ", which will be charged to the card " + cardNumber
+    return "Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + "."
   } else {
     return "Sorry, we don't have a credit card on file for you."
   }
